6a5e3adec429dc56557739c41bac1c9d8ec96645
Pedro Barros <pedrobars7a@gmail.com>
2022-09-29 07:41:46 -0300

    Dados no template

--- src/app/components/first-component/first-component.component.ts
+++ src/app/components/first-component/first-component.component.ts
@@ -3,13 +3,19 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-first-component',
   templateUrl: './first-component.component.html',
-  styleUrls: ['./first-component.component.css']
+  styleUrls: ['./first-component.component.css'],
 })
 export class FirstComponentComponent implements OnInit {
+  name: string = 'Pedro';
+  age: number = 30;
+  job = 'Programador';
+  hobbies = ['Correr', 'Jogar', 'Estudar'];
+  car = {
+    name: 'HB20s',
+    year: '2021',
+  };
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
