9358f6ebb36817ac278530168e7e65680dab58a1
Pedro Barros <pedrobars7a@gmail.com>
2022-09-29 09:14:40 -0300

    Diretivas do Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { FirstComponentComponent } from './components/first-component/first-component.component';
 import { ParentsDataComponent } from './components/parents-data/parents-data.component';
+import { DirectivesComponent } from './components/directives/directives.component';
 
 @NgModule({
-  declarations: [AppComponent, FirstComponentComponent, ParentsDataComponent],
+  declarations: [AppComponent, FirstComponentComponent, ParentsDataComponent, DirectivesComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
