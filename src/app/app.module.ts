ec89d92ee4a621eac1afcfe360bd16acf67405ca
Pedro Barros <pedrobars7a@gmail.com>
2022-09-29 09:47:54 -0300

    Renderização condicional

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,10 @@ import { AppComponent } from './app.component';
 import { FirstComponentComponent } from './components/first-component/first-component.component';
 import { ParentsDataComponent } from './components/parents-data/parents-data.component';
 import { DirectivesComponent } from './components/directives/directives.component';
+import { IfRenderComponent } from './components/if-render/if-render.component';
 
 @NgModule({
-  declarations: [AppComponent, FirstComponentComponent, ParentsDataComponent, DirectivesComponent],
+  declarations: [AppComponent, FirstComponentComponent, ParentsDataComponent, DirectivesComponent, IfRenderComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
